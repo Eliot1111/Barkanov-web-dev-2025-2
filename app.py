8c0424da239510b4bcc1f7c9e4913de9fd846f73
Matthew Barkanov <73409551+Eliot1111@users.noreply.github.com>
2025-05-09 15:33:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from lab1.app.app import app as lab1_app
 from lab2.app.app import app as lab2_app
 from lab3.app import app as lab3_app
 from lab4.app import app as lab4_app
+from lab5.app import app as lab5_app
 from root_app.app import app as root_app
 
 
@@ -10,6 +11,7 @@ app = DispatcherMiddleware(root_app, {
     '/lab1': lab1_app,
     '/lab2': lab2_app,
     '/lab3': lab3_app,
-    '/lab4': lab4_app
+    '/lab4': lab4_app,
+    '/lab5': lab5_app
 })
 application = app
