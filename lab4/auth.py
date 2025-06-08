3be2d2cd162e1440cffafd05880bdde9f9ec880b
Matthew Barkanov <73409551+Eliot1111@users.noreply.github.com>
2025-06-08 14:12:31 +0300

    Update auth.py

--- lab4/auth.py
+++ lab4/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
-from models import User
+from lab4.models import User
 from lab4.extensions import db, login_manager
 from forms import LoginForm, ChangePasswordForm
 
