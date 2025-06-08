f2fe3be8f65444256048b7b54e4623cc9805797c
Matthew Barkanov <73409551+Eliot1111@users.noreply.github.com>
2025-06-08 15:29:14 +0300

    Update courses.py

--- lab6/app/courses.py
+++ lab6/app/courses.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
 
-from app.models import db
-from app.repositories import CourseRepository, UserRepository, CategoryRepository, ImageRepository
+from lab6.app.models import db
+from lab6.app.repositories import CourseRepository, UserRepository, CategoryRepository, ImageRepository
 
 user_repository = UserRepository(db)
 course_repository = CourseRepository(db)
