e0067e8ba29c201dbd58de17a25547bfa3573b22
Matthew Barkanov <73409551+Eliot1111@users.noreply.github.com>
2025-06-08 14:03:04 +0300

    Update auth.py

--- lab4/auth.py
+++ lab4/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from models import User
-from extensions import db, login_manager
+from lab4.extensions import db, login_manager
 from forms import LoginForm, ChangePasswordForm
 
 auth = Blueprint('auth', __name__)
