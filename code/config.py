b1da8e80809e57cd0009082c2a891db98a1b4748
Matthew Barkanov <73409551+Eliot1111@users.noreply.github.com>
2025-06-16 22:32:38 +0300

    Update config.py

--- code/config.py
+++ code/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'super-secret-key-for-csrf'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:7004/vmshop'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Matthew12:Matvey2005@localhost/vmshop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
